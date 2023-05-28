b5bd1ff681d380bdaf4c6b744044d39000c3f58b
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-05-28 22:24:24 +0800

    feat:tambahan menu regis + revisi home

--- Dietaria/app/src/main/java/dietaria/scenes/HomeScene.java
+++ Dietaria/app/src/main/java/dietaria/scenes/HomeScene.java
@@ -1,10 +1,13 @@
 package dietaria.scenes;
 
+import dietaria.scenes.RegisterMenu1;
+
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -18,14 +21,29 @@ public class HomeScene {
 
     public void show() {
         StackPane layout = new StackPane();
+        Scene scene = new Scene(layout,340, 649);
         ImageView bg = new ImageView("/images/bgHome.png");
         layout.getChildren().add(bg);
+        bg.setFitHeight(scene.getHeight());
+        bg.setFitWidth(scene.getWidth());
 
+        Button logo = new Button();
+        ImageView dietaria = new ImageView("/images/DietariaLogo1.png");
+        logo.setGraphic(dietaria);
+        HBox hBox = new HBox(logo);
+        hBox.setAlignment(Pos.TOP_CENTER);
+        logo.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+        layout.getChildren().add(hBox);
+        
         ImageView register = new ImageView("/images/btnRegister.png"); 
         Button btnRegister = new Button();
         btnRegister.setGraphic(register);
         btnRegister.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
         btnRegister.setPrefSize(200, 25);
+        btnRegister.setOnAction(e -> {
+            RegisterMenu1 daftar = new RegisterMenu1(stage);
+            daftar.show();
+        });
 
         ImageView login = new ImageView("/images/btnLogin.png"); 
         Button btnLogin = new Button();
@@ -37,10 +55,8 @@ public class HomeScene {
         homeButton.setSpacing(15);
         homeButton.setAlignment(Pos.BOTTOM_CENTER);
         layout.getChildren().add(homeButton);
-
         StackPane.setMargin(homeButton, new Insets(0, 0, 50, 0));
 
-        Scene scene = new Scene(layout,340, 649);
         stage.setScene(scene);
         stage.show();
     }
--- Dietaria/app/src/main/java/dietaria/scenes/RegisterMenu1.java
+++ Dietaria/app/src/main/java/dietaria/scenes/RegisterMenu1.java
@@ -1,5 +1,24 @@
 package dietaria.scenes;
 
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class RegisterMenu1 {
-    
+    private Stage stage;
+
+    public RegisterMenu1(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void show() {
+        StackPane layout = new StackPane();
+        ImageView bg = new ImageView("/images/bgHome.png");
+        layout.getChildren().add(bg);
+
+        Scene scene = new Scene(layout,340, 649);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
