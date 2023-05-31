698f7e7745501537c66b58a8626269f25d1279bf
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-05-31 22:52:58 +0800

    Feat:menu makanan&aktivitas (smua kerja kak sumph)

--- Dietaria/app/src/main/java/dietaria/scenes/TambahAktivitas.java
+++ Dietaria/app/src/main/java/dietaria/scenes/TambahAktivitas.java
@@ -1,11 +1,17 @@
 package dietaria.scenes;
 
 import dietaria.models.aktivitas;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class TambahAktivitas {
@@ -18,7 +24,7 @@ public class TambahAktivitas {
     public void show() {
         StackPane layout = new StackPane();
         Scene scene = new Scene(layout,340, 649);
-        ImageView bg = new ImageView("/images/bg.png");
+        ImageView bg = new ImageView("/images/bgTambahAktivitas.png");
         layout.getChildren().add(bg);
         bg.setFitHeight(scene.getHeight());
         bg.setFitWidth(scene.getWidth());
@@ -28,6 +34,48 @@ public class TambahAktivitas {
         dropShadow.setOffsetY(2);
         dropShadow.setColor(javafx.scene.paint.Color.GRAY);
 
+        Label nama = new Label("Nama Aktivitas");
+        nama.setFont(Font.font("Bitter", 15));
+        TextField name = new TextField();
+        name.setPromptText("Nama Aktivitas");
+        name.setEffect(dropShadow);
+        name.getStyleClass().add("text-field");
+        VBox vBoxNama = new VBox(nama, name);
+        vBoxNama.setSpacing(5);
+        vBoxNama.setPadding(new Insets(0, 0, 0, 45));
+        
+        Label kaloriDibakar = new Label("Kalori Dibakar");
+        kaloriDibakar.setFont(Font.font("Bitter", 15));
+        TextField kalori = new TextField();
+        kalori.setPromptText("Kalori yang dibakar");
+        kalori.setEffect(dropShadow);
+        kalori.getStyleClass().add("text-field");
+        VBox vBoxkalori = new VBox(kaloriDibakar, kalori);
+        vBoxkalori.setSpacing(5);
+        vBoxkalori.setPadding(new Insets(0, 0, 0, 45));
+
+        VBox vBox = new VBox(vBoxNama, vBoxkalori);
+        vBox.setSpacing(20);
+
+        Button tambah = new Button();
+        ImageView btnTambah = new ImageView("/images/btnTambah.png");
+        tambah.setGraphic(btnTambah);
+        tambah.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+
+        Button balik = new Button();
+        ImageView buttonBalik = new ImageView("/images/btnBack.png");
+        balik.setGraphic(buttonBalik);
+        balik.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+
+        VBox tombol = new VBox(tambah, balik);
+        tombol.setPadding(new Insets(0, 0, 0, 40));
+
+        VBox everything = new VBox(vBox, tombol);
+        everything.setSpacing(30);
+        everything.setAlignment(Pos.CENTER);
+        layout.getChildren().add(everything);
+
+        scene.getStylesheets().add("/style/TextFieldStyle.css");
         stage.setScene(scene);
         stage.show();
     }        
--- Dietaria/app/src/main/java/dietaria/scenes/TambahMakanan.java
+++ Dietaria/app/src/main/java/dietaria/scenes/TambahMakanan.java
@@ -1,11 +1,16 @@
 package dietaria.scenes;
 
 import dietaria.models.makanan;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TambahMakanan {
@@ -18,7 +23,7 @@ public class TambahMakanan {
     public void show() {
         StackPane layout = new StackPane();
         Scene scene = new Scene(layout,340, 649);
-        ImageView bg = new ImageView("/images/bg.png");
+        ImageView bg = new ImageView("/images/bgTambahMakanan.png");
         layout.getChildren().add(bg);
         bg.setFitHeight(scene.getHeight());
         bg.setFitWidth(scene.getWidth());
@@ -28,6 +33,63 @@ public class TambahMakanan {
         dropShadow.setOffsetY(2);
         dropShadow.setColor(javafx.scene.paint.Color.GRAY);
 
+        TextField name = new TextField();
+        name.setPromptText("Nama Makanan");
+        name.setEffect(dropShadow);
+        name.getStyleClass().add("text-field");
+
+        TextField review = new TextField();
+        review.setPromptText("Review Makanan");
+        review.setEffect(dropShadow);
+        review.getStyleClass().add("text-field");
+
+        VBox namaMakanan = new VBox(name, review);
+        namaMakanan.setSpacing(10);
+        layout.getChildren().add(namaMakanan);
+
+        TextField kalori = new TextField();
+        kalori.setPromptText("Kalori");
+        kalori.setEffect(dropShadow);
+        kalori.getStyleClass().add("text-field");
+
+        TextField protein = new TextField();
+        protein.setPromptText("Protein");
+        protein.setEffect(dropShadow);
+        protein.getStyleClass().add("text-field");
+
+        TextField karbo = new TextField();
+        karbo.setPromptText("Karbohidrat");
+        karbo.setEffect(dropShadow);
+        karbo.getStyleClass().add("text-field");
+        
+        TextField lemak = new TextField();
+        lemak.setPromptText("Lemak");
+        lemak.setEffect(dropShadow);
+        lemak.getStyleClass().add("text-field");
+
+        VBox nutrisi = new VBox(kalori, protein, karbo, lemak);
+        nutrisi.setSpacing(15);
+
+        Button tambah = new Button();
+        ImageView btnTambah = new ImageView("/images/btnTambah.png");
+        tambah.setGraphic(btnTambah);
+        tambah.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+
+        Button balik = new Button();
+        ImageView buttonBalik = new ImageView("/images/btnBack.png");
+        balik.setGraphic(buttonBalik);
+        balik.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+
+        VBox tombol = new VBox(tambah, balik);
+        tombol.setPadding(new Insets(0, 0, 0, 0));
+
+        VBox everything = new VBox(namaMakanan, nutrisi, tombol);
+        everything.setSpacing(15);
+        everything.setAlignment(Pos.CENTER);
+        everything.setPadding(new Insets(15, 0, 0, 40));
+        layout.getChildren().addAll(everything);
+
+        scene.getStylesheets().add("/style/TextFieldStyle.css");
         stage.setScene(scene);
         stage.show();
     }
